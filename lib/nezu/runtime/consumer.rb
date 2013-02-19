42d687ffeff69c8ef3efc1bdcedb2cb2143af708
Sascha Teske <sascha.teske@gmail.com>
2013-02-19 16:30:32 +0100

    require app files; use pre and postfixes for queues

--- lib/nezu/runtime/consumer.rb
+++ lib/nezu/runtime/consumer.rb
@@ -6,7 +6,10 @@ module Nezu
       end
 
       def self.to_queue_name
-        self.to_s.gsub(/::/, '.').underscore
+        q = ''
+        q << "#{configatron.amqp.queue_prefix}." if configatron.amqp.queue_prefix
+        q << self.to_s.gsub(/::/, '.').underscore
+        q << ".#{configatron.amqp.queue_postfix}" if configatron.amqp.queue_postfix
       end
 
       def handle_message(metadata, payload)
