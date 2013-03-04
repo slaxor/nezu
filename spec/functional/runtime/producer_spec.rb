5775d180b8e04c7cec1b4878004f5d2da91bab35
Sascha Teske <sascha.teske@gmail.com>
2013-03-04 17:47:42 +0100

    Fix specs

--- spec/functional/runtime/producer_spec.rb
+++ spec/functional/runtime/producer_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 require 'nezu/runtime/producer'
 describe Nezu::Runtime::Producer do
+  before do
+    #Bunny.stub!(:new).and_return(AlwaysHappy.new)
+  end
+
   describe '::push!' do
     it 'should create a new message on the server' do
       configatron.amqp.url = 'amqp://127.0.0.1'
+      Bunny.should_receive(:new).with(configatron.amqp.url).and_return(AlwaysHappy.new)
       module ExampleProducers;class MyQueue<Nezu::Runtime::Producer;end;end
       ExampleProducers::MyQueue.push!(:foo => 'bar')
     end
