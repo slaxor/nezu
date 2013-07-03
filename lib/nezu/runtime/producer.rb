05f9215a96ffd99d5fe8fb3d75a5417019bb3740
Sascha Teske <sascha.teske@gmail.com>
2013-07-03 17:07:03 +0200

    Make a non-threaded connection

--- lib/nezu/runtime/producer.rb
+++ lib/nezu/runtime/producer.rb
@@ -3,16 +3,13 @@ module Nezu
     class Producer
       extend Nezu::Runtime::Common
 
-      def self.inherited(subclass)
-        super
-        @@connection = Bunny.new(configatron.amqp.send(Nezu.env.to_sym).url)
-        @@connection.start
-        @@channel = @@connection.create_channel
-      end
-
       def self.push!(params = {})
-        @@queue  ||= @@channel.queue(queue_name)
-        @@queue.publish(params.to_json, :content_type => 'application/json')
+        connection = Bunny.new(configatron.amqp.send(Nezu.env.to_sym).url, :threaded => false)
+        connection.start
+        channel = connection.create_channel
+        queue ||= channel.queue(queue_name)
+        queue.publish(params.to_json, :content_type => 'application/json')
+        connection.close
       end
     end
   end
