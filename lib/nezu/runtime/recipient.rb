e5b4dfae2cbd74f0e0145b901b389e5cc8134cc8
Sascha Teske <sascha.teske@gmail.com>
2014-06-03 15:05:27 +0200

    Add better error message

--- lib/nezu/runtime/recipient.rb
+++ lib/nezu/runtime/recipient.rb
@@ -9,13 +9,23 @@ module Nezu
     class RecipientError < RuntimeError
       def self.does_not_exist(q)
         klass = q.sub(/^#{Nezu::Config.amqp.development.queue_prefix}./, '').sub(/.#{Nezu::Config.amqp.development.queue_postfix}$/,'').classify
-        message = %Q(
-          The class "#{klass}" doesn`t exist or is not a child of "Nezu::Runtime::Producer".
-          Please create one in \"app/producers/#{klass.underscore}.rb\" with the content of at least:
+        all_producers = Nezu::Runtime::Producer.descendants.inject({}) {|accu, producer| accu.merge({producer.name => producer.queue_name})}
 
+        message = %Q(
+
+          I couldn't find the queue "#{q}" anywhere in the producers.
+          Please create one in \"app/producers/#{klass.underscore}.rb\" with the content of at least:
+          -----------8<-----------8<-----------8<-----------
           class #{klass} < Nezu::Runtime::Producer
           end
+          ----------->8----------->8----------->8-----------
+          These are the producers i've got:
+
+
         ).gsub(/^\s*/, '')
+        all_producers.each do |p,q|
+          message << %Q(class: "#{p}" queue_name:  "#{q}"\n)
+        end
         self.new(message)
       end
 
